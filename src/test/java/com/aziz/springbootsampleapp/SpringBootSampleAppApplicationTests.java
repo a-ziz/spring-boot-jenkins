9ac7702b67fe82f748942329be5c58a199ed59d0
Aziz <azizahmed.dev@gmail.com>
2024-04-23 14:26:35 -0400

    commit msg

---
+++ src/test/java/com/aziz/springbootsampleapp/SpringBootSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziz.springbootsampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
